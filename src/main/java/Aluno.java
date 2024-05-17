a2b565577032af9480baee59437b8156830867a5
Isaias <65170430+isaiasfce@users.noreply.github.com>
2024-05-17 20:26:13 -0300

    Update Aluno.java
    Adicionei o método para obter uma representação do tipo String da classe aluno.

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -22,4 +22,12 @@ public class Aluno extends Pessoa {
     public void pagarMensalidade() {
         System.out.println("Este aluno: " + nome + ", pagou a mensalidade.");
     }
+
+    /* Sobrescrita do método toString. Ele é chamado automaticamente.
+    Serve para obter uma representação String  de um objeto. Aqui ele
+    é usado para listar o nome dos alunos na lista de alunos criada na classe Turma*/
+    @Override
+    public String toString(){
+        return nome;
+    }
 }
